849da06af5284cd3a3ca3a53aa3c050e84b593c5
Kayla Braun <go1018bx@go.minneapolis.edu>
2017-09-04 17:04:28 -0500

    object creating

--- seed.js
+++ seed.js
@@ -14,6 +14,7 @@ function seed() {
             var TYPES = {
                 FSQUAT: frontSquatId,
                 BSQUAT: backSquatId
+
             };
             var dayOne = {
                 sets: [{
